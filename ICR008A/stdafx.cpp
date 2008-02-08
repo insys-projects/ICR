5c6ce0f89ae527920762ca3acffbc077b1bc53d3
Ekkore <Ekkore@a8a5cdc4-a91d-e646-800f-a4054892b1cc>
2008-02-08 10:05:30 +0000

    Ремонт хранилища

--- ICR008A/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// Icr008A.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
