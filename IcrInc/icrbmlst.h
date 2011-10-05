596fd53b8a796b405bc267415d2e9edb5dba2e7c
andor <andor@a8a5cdc4-a91d-e646-800f-a4054892b1cc>
2011-10-05 10:48:53 +0000

    добавил FMC105P

--- IcrInc/icrbmlst.h
+++ IcrInc/icrbmlst.h
@@ -61,7 +61,8 @@ typedef enum _ICR_BMType
 		SYNC8CHPCI	= 0x50A2,
 		DR16        = 0x53B0,
 		SYNCCP6		= 0x53A2,
-		AMBPEX5		= 0x5507
+		AMBPEX5		= 0x5507,
+		FMC105P		= 0x5509
 } ICR_BMType;
 
 #endif	// __ICRBMLST_H_ 
