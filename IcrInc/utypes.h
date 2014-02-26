e81071160c3ca5578b2c55b1af1c85fe206a6050
andor <andor@a8a5cdc4-a91d-e646-800f-a4054892b1cc>
2014-02-26 13:27:52 +0000

    utypes.h equ from brdinc

--- IcrInc/utypes.h
+++ IcrInc/utypes.h
@@ -10,6 +10,7 @@
 #ifndef _UTYPES_H_
 #define _UTYPES_H_
 
+#include "utypes_linux.h"
 
 /*************************************
 *
@@ -51,7 +52,7 @@ typedef unsigned long   HANDLE;
 */
 
 
-#if defined(_Windows) || defined(_WIN32)
+#ifdef _WIN32
 
 typedef	unsigned char	UINT08;
 typedef	signed   char	SINT08;
@@ -85,6 +86,7 @@ typedef wchar_t			BRDCHAR;
 #define BRDC_strcmp		wcscmp
 #define BRDC_stricmp	_wcsicmp
 #define BRDC_strnicmp	wcsnicmp
+#define BRDC_strncmp	wcsncmp
 #define BRDC_strcat		wcscat
 #define BRDC_strchr		wcschr
 #define BRDC_strstr		wcsstr
@@ -94,18 +96,37 @@ typedef wchar_t			BRDCHAR;
 #define BRDC_atol		_wtol
 #define BRDC_atoi		_wtoi
 #define BRDC_atoi64		_wtoi64
+#define BRDC_strtoui64	_wcstoui64
 #define BRDC_atof		_wtof
+#define BRDC_strlwr		_wcslwr
+#define BRDC_itoa		_itow
 #define BRDC_printf		wprintf
 #define BRDC_fprintf	fwprintf
+#define BRDC_fscanf		fwscanf
 #define BRDC_sprintf	swprintf
 #define BRDC_vsprintf	vswprintf
 #define BRDC_sscanf		swscanf
 #define BRDC_fopen		_wfopen
 #define BRDC_sopen		_wsopen
+#define BRDC_open		_wopen
 #define BRDC_fgets		fgetws
 #define BRDC_fputs		fputws
+#define BRDC_fgetc		fgetwc
 #define BRDC_getenv		_wgetenv
 #define BRDC_main		wmain
+#define BRDC_toupper	towupper
+#define BRDC_tolower	towlower
+#define BRDC_isspace	iswspace
+#define BRDC_isdigit	iswdigit
+#define BRDC_isalnum	iswalnum
+#define BRDC_isxdigit	iswxdigit
+#define BRDC_cputs      _cputws
+#define BRDC_cscanf     _cwscanf
+#define BRDC_fputchar   _fputwchar
+#define BRDC_fgetchar   _fgetwchar
+#define BRDC_flushall   flushall
+#define BRDC_putch      _putwch
+#define BRDC_cgets      _cgetws
 #else
 typedef char			BRDCHAR;
 #define _BRDC(x)		x
@@ -115,6 +136,7 @@ typedef char			BRDCHAR;
 #define BRDC_strcmp		strcmp
 #define BRDC_stricmp	_stricmp
 #define BRDC_strnicmp	_strnicmp
+#define BRDC_strncmp	strncmp
 #define BRDC_strcat		strcat
 #define BRDC_strchr		strchr
 #define BRDC_strstr		strstr
@@ -124,21 +146,40 @@ typedef char			BRDCHAR;
 #define BRDC_atol		atol
 #define BRDC_atoi		atoi
 #define BRDC_atoi64		_atoi64
+#define BRDC_strtoui64	_strtoui64
 #define BRDC_atof		atof
+#define BRDC_strlwr		_strlwr
+#define BRDC_itoa		_itoa
 #define BRDC_printf		printf
 #define BRDC_fprintf	fprintf
+#define BRDC_fscanf		fscanf
 #define BRDC_sprintf	sprintf
 #define BRDC_vsprintf	vsprintf
 #define BRDC_sscanf		sscanf
 #define BRDC_fopen		fopen
 #define BRDC_sopen		sopen
+#define BRDC_open		_open
 #define BRDC_fgets		fgets
 #define BRDC_fputs		fputs
+#define BRDC_fgetc		fgetc
 #define BRDC_getenv		getenv
 #define BRDC_main		main
+#define BRDC_toupper	toupper
+#define BRDC_tolower	tolower
+#define BRDC_isspace	isspace
+#define BRDC_isdigit	isdigit
+#define BRDC_isalnum	isalnum
+#define BRDC_isxdigit	isxdigit
+#define BRDC_cputs      cputs
+#define BRDC_cscanf     cscanf
+#define BRDC_fputchar   fputchar
+#define BRDC_fgetchar   fgetchar
+#define BRDC_flushall   flushall
+#define BRDC_putch      putch
+#define BRDC_cgets      cgets
 #endif
 
-#endif /* _Windows */
+#endif /* _WIN32 */
 
 
 /*************************************
@@ -277,40 +318,6 @@ typedef UINT32	USHORT;
 typedef UINT32	UCHAR;
 
 #endif /* __ADSPTS__ */
-
-/*************************************
-*
-*  Types for MC24
-*/
-
-#if defined(__GNUC__)
-
-typedef unsigned char   UINT08;
-typedef signed   char   SINT08;
-typedef unsigned short  UINT16;
-typedef signed   short  SINT16;
-typedef unsigned long   UINT32;
-typedef signed   long   SINT32;
-
-typedef unsigned char   U08, *PU08;
-typedef signed   char   S08, *PS08;
-typedef unsigned short  U16, *PU16;
-typedef signed   short  S16, *PS16;
-typedef unsigned long   U32, *PU32;
-typedef signed   long   S32, *PS32;
-
-typedef unsigned long long int	U64, *PU64;
-typedef signed   long long int	S64, *PS64;
-
-typedef float           REAL32, *PREAL32;
-typedef double          REAL64, *PREAL64;
-
-typedef unsigned char   UCHAR;
-typedef unsigned short  USHORT;
-typedef unsigned long   ULONG;
-typedef unsigned long   HANDLE;
-
-#endif /* __GNUC__ */
 
 /*************************************
 *
