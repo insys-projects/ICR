285d798d96ba9f1789e21177ace826d5ef3db3d1
andor <andor@a8a5cdc4-a91d-e646-800f-a4054892b1cc>
2014-05-15 07:54:57 +0000

    исправил имя структуры и комментарии

--- IcrInc/icrxm416x250m.h
+++ IcrInc/icrxm416x250m.h
@@ -16,17 +16,17 @@
 
 const USHORT XM416x250M_CFG_TAG = 0x5516; // тэг для структуры конфигурационных параметров базовой части комбинированоого модуля XM416x250M
 
-// FMC105P/FMC106P modules configuration
-// Конфигурационные параметры базовых модулей FMC105P/FMC106P
-typedef struct _ICR_CfgFmc105p {
-	U16	wTag;			// тэг структуры (FMC105P_CFG_TAG или FMC106P_CFG_TAG)
+// XM416x250M module configuration
+// Конфигурационные параметры базового модуля XM416x250M
+typedef struct _ICR_CfgXm416x250m {
+	U16	wTag;			// тэг структуры (XM416x250M_CFG_TAG)
 	U16	wSize;			// размер всех следующих полей структуры
 	U08	bAdmIfCnt;		// количество интерфейсов ADM
 	U32	dSysGen;			// Системный генератор (Гц) (по умолчанию 250 МГц)
 	U08	isDDR3;			// Наличие памяти DDR3
 	U08	isVoltageMonitor;	// Наличие монитора напряжений
 	U08	isCurrentMonitor;	// Наличие монитора тока
-} ICR_CfgFmc105p, *PICR_CfgFmc105p, ICR_Cfg5509, *PICR_Cfg5509;
+} ICR_CfgXm416x250m, *PICR_CfgXm416x250m, ICR_Cfg5516, *PICR_Cfg5516;
 
 #pragma pack(pop)    
 
