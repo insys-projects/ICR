caa2b587a5d033253457c06369455405c033e7b9
dor <dor@a8a5cdc4-a91d-e646-800f-a4054892b1cc>
2015-07-07 17:01:22 +0000

    Убраны "лишние" поля

--- IcrInc/Icr6678.h
+++ IcrInc/Icr6678.h
@@ -13,6 +13,7 @@
 //
 //  History:
 //  19-10-12 - builded
+//  06-07-15 - modify (minimize)
 //
 //*******************************************************************
 #ifndef __ICR6678_H
@@ -20,16 +21,17 @@
 
 #pragma pack(push, 1)    
 
+const USHORT FM408C_CFG_TAG      = 0x6608; // тэг для структуры конфигурационных параметров базового модуля FM408C
 const USHORT FMC110_6678_CFG_TAG = 0x6610; // тэг для структуры конфигурационных параметров базового модуля FMC110
 const USHORT FMC112_6678_CFG_TAG = 0x6612; // тэг для структуры конфигурационных параметров базового модуля FMC112
 const USHORT FMC114_6678_CFG_TAG = 0x6614; // тэг для структуры конфигурационных параметров базового модуля FMC114
 const USHORT FMC117_6678_CFG_TAG = 0x6617; // тэг для структуры конфигурационных параметров базового модуля FMC117
 const USHORT PEX_SRIO_CFG_TAG	 = 0x6615; // тэг для структуры конфигурационных параметров базового модуля PEX-SRIO
+const USHORT DSP6678PEX_CFG_TAG	 = 0x6620; // тэг для структуры конфигурационных параметров базового модуля DSP6678PEX
 
 // 6678 module configuration
-// Конфигурационная структура базового модуля 6678
 typedef struct _ICR_Cfg6678 {
-	U16	wTag;			// tag of structure (0x6610, 0x6612 ...)
+	U16	wTag;			// tag of structure 0x6678
 	U16	wSize;			// size of all following fields of structure (without wTag + wSize) = sizeof(ICR_XXXX) - 4
 	
 	U08	bCpuCnt;		// число процессоров на модуле
@@ -57,62 +59,8 @@ typedef struct _ICR_Cfg6678 {
 
 	U32	dNandSize;		// Размер FLASH памяти типа NAND (EMIF16)
 	U32	dNorSize;		// Размер FLASH памяти типа NOR (SPI)
+
 	U32	dSizeOfDDR3;	// Размер DDR3 (32-битные слова), если запаян на модуле
-
-    // LEVELING для памяти DDR3
-	U16 wWrLev0;
-	U16 wWrLev1;
-	U16 wWrLev2;
-	U16 wWrLev3;
-	U16 wWrLev4;
-	U16 wWrLev5;
-	U16 wWrLev6;
-	U16 wWrLev7;
-	U16 wWrLev8;
-
-	U16 wGtLev0;
-	U16 wGtLev1;
-	U16 wGtLev2;
-	U16 wGtLev3;
-	U16 wGtLev4;
-	U16 wGtLev5;
-	U16 wGtLev6;
-	U16 wGtLev7;
-	U16 wGtLev8;
-
-	// ВременнЫе параметры DDR3
-	U08	bT_RP;
-	U08	bT_RCD;
-	U08	bT_WR;
-	U08	bT_RAS;
-	U08	bT_RC;
-	U08	bT_RRD;
-	U08	bT_WTR;
-	U08	bT_RTP;
-	U08	bT_RFC;
-
-	// Прочие параметры DDR3
-	U08	bCL;
-	U08	bROWSIZE;
-	U08	bRANK;
-	U08	bPSIZE;
-
-	// frequence multipliers and dividers
-	U08 bMainPllMult;   
-    U08 bMainPllDiv;	
-
-	U08	bDdr3PllMul;
-	U08	bDdr3PllDiv;
-
-	U08 bNetPllMult;   
-    U08 bNetPllDiv;	
-
-	// Network parameters
-    U16 wNet0AdviseAbility;
-    U16 wNet0AuxConfig;
-    U16 wNet1AdviseAbility;
-    U16 wNet1AuxConfig;
-
 
 } ICR_Cfg6678, *PICR_Cfg6678;
 
